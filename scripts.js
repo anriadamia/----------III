664f4cbfad6ce7757add4b2322712405d91c04d5
anriadamia <anriadamia2000@gmail.com>
2022-10-25 09:27:04 +0400

    Practice III

---
+++ scripts.js
@@ -0,0 +1,59 @@
+// let numbers = [5, 25, 89, 120, 36];
+// numbers.push("javascript", "python");
+// numbers.unshift("html", "css");
+// console.log(numbers);
+// console.log(numbers.length);
+// numbers.pop();
+// numbers.shift();
+// console.log(numbers);
+
+// let items=["orange","banana", "pear"];
+// console.log(items.length);
+// items.push("apple","pineapple");
+// items.unshift("watermelon")
+// console.log(items.length);
+// items.splice(2,0,"mango")
+// console.log(items);
+// items.pop();
+// items.shift();
+// console.log(items);
+// console.log(items.length);
+
+// let array = [12, 25, 3, 6, 8, 14, 7, 23];
+// let newArray=array.map((item)=>item/3);
+// console.log(newArray);
+
+// let languages=["html", "css", "javascripts", "python", "php"];
+// let newLanguages=languages.filter((item)=>item.length>3)
+// console.log(newLanguages);
+
+// let item=["12", "google", 32,"null", "yahoo",25];
+
+// let words = ['Madrid', 'Rome', 'Milan', 'Berlin'];
+// let newWords=words.filter((item)=>item.includes("m") || item.includes("M"))
+// console.log(newWords);
+
+// let arr1 = [1, 2];
+// let arr2 = [3, 4];
+// let arr3 = [5, 6];
+// let newArray = arr1.concat(arr2,arr3);
+// console.log(newArray);
+
+
+// let numbers = [-2, 5, 10, -5, -1, 1, 3, 4, -15];
+// let newNumbers=numbers.filter((items)=>items>0);
+// console.log(newNumbers);
+
+// let arr = [-1, -2, -3, 4];
+// let newArray=arr.some((item)=>item>0);
+// console.log(newArray);
+
+
+// let array =[23,45,32,5,87,7,3,98];
+// let newArray=array.sort((x,y)=>y-x)[array.length-1]
+// console.log(newArray);
+
+
+// let array =[1, 2, 3, 4, 5];
+// array.splice(3,0,"a","b","c")
+// console.log(array);
